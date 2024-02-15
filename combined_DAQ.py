1f72f838fa0340869673614131392e28b6db234e
joongwonlee1 <113049301+joongwonlee1@users.noreply.github.com>
2024-02-14 16:29:35 -0800

    Add files via upload

---
+++ combined_DAQ.py
@@ -0,0 +1,52 @@
+import adafruit_bme680
+import time
+import board
+import pandas as pd
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+import sys
+
+
+reset_pin = None
+import serial
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+
+print("Found PM2.5 sensor, reading data...")
+
+i2c= board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+bme680.sea_level_pressure = 1013.25
+#start = time.time()
+lst = []
+
+
+runtime = int(sys.argv[1])
+filename = sys.argv[2]
+
+for i in range(runtime):
+    time.sleep(1)
+    try:
+        aqdata = pm25.read()
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+    sublst = []
+    sublst.append(time.time())
+    sublst.append("%0.1f" % bme680.temperature)
+    sublst.append("%d" % bme680.gas)
+    sublst.append("%0.1f" % bme680.relative_humidity)
+    sublst.append("%0.3f" % bme680.pressure)
+    sublst.append("%0.2f" %bme680.altitude)
+    sublst.append("%0.1f" % aqdata["particles 10um"])
+    sublst.append("%d" % aqdata["particles 25um"])
+    sublst.append("%0.1f" % aqdata["particles 100um"])
+    lst.append(sublst)
+
+
+col_names = ["Time","Temperature", "Gas","Humidity", "Pressure", "Altitude","pm10 standard", "pm25 standard","pm100 env"]
+df = pd.DataFrame(lst, columns = col_names)
+csv_data = df.to_csv(filename, index = False)
