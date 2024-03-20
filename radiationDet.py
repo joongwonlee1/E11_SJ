6c8f00895ebb070acb64263572def54737427d6f
joongwonlee1 <113049301+joongwonlee1@users.noreply.github.com>
2024-03-20 15:18:34 -0700

    Add files via upload

--- radiationDet.py
+++ radiationDet.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 lst = []
 channel = 17
+last_time = time.time()
+event_counter = 0
+last_list_size =0
+increase_counter =0
+last_increase_time = time.time()
+
 def my_callback(channel):
     global lst
     if GPIO.input(channel) == GPIO.LOW:
@@ -12,11 +18,13 @@ def my_callback(channel):
     else:
         print('\n▼  at ' + str(time.time()))
         lst.append((time.time(), "HIGH"))
+        event_counter +=1
 
 try:
     runtime = int(input("Enter runtime in seconds: "))
+    count_interval = int(input("Enter the counting interval: "))
     filename = input("Enter output filename (without extension): ")
-
+    event_counter = 0
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(17, GPIO.BOTH, callback=my_callback)
@@ -25,6 +33,22 @@ try:
     start_time = time.time()
 
     while (time.time() - start_time) < runtime:
+        if time.time() - last_time >= count_interval:
+            lst.append(("Count Interval", event_counter))
+            last_rest_time = time.time()
+        
+        if len(lst) > last_list_size:
+            increase_counter +=1
+            last_list_size = len(lst)
+            
+        if time.time() - last_increase_time >= count_interval:
+            print("increase counter")
+            increase_counter =0
+            last_increase_time = time.time()
+            
+        time.sleep(runtime - (time.time() - start_time()))
+            
+        
         pass  # Wait until runtime is reached
 
 finally:
@@ -35,5 +59,6 @@ print("Writing data to CSV...")
 col_names = ["Time", "State"]
 df = pd.DataFrame(lst, columns=col_names)
 df.to_csv(filename + ".csv", index=False)
+
 
 print("Data written to", filename + ".csv")
