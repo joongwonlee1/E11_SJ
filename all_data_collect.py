23f60d618b0dec1b295f81f23645710bb727c8ec
Sophie-Hilsabeck <sophie.hilsabeck0@gmail.com>
2024-04-03 16:51:14 -0700

    Update all_data_collect.py

--- all_data_collect.py
+++ all_data_collect.py
@@ -129,62 +129,57 @@ import time
 import pandas as pd
 
 lst = []
+
 channel = 17
-last_time = time.time()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+last_interval_time = time.time()
 event_counter = 0
-last_list_size =0
-increase_counter =0
-last_increase_time = time.time()
+interval_number = 0
 
 def my_callback(channel):
-    global lst
+    global event_counter
     if GPIO.input(channel) == GPIO.LOW:
-        print('\n ▲ at ' + str(time.time()))
-        lst.append((time.time(), "LOW"))
+        print('\n▲ LOW at ' + str(time.time()))
     else:
-        print('\n▼  at ' + str(time.time()))
-        lst.append((time.time(), "HIGH"))
-        event_counter +=1
+        print('\n▼ HIGH at ' + str(time.time()))
+    event_counter += 1
 
 try:
     runtime = int(input("Enter runtime in seconds: "))
     count_interval = int(input("Enter the counting interval: "))
     filename = input("Enter output filename (without extension): ")
-    event_counter = 0
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(17, GPIO.BOTH, callback=my_callback)
+    
+    GPIO.add_event_detect(channel, GPIO.BOTH, callback=my_callback)
 
-    input('\nPress Enter to start recording...\\n')
     start_time = time.time()
 
-    while (time.time() - start_time) < runtime:
-        if time.time() - last_time >= count_interval:
-            lst.append(("Count Interval", event_counter))
-            last_rest_time = time.time()
+    while (time.time() - start_time) < runtime + count_interval: 
+        current_time = time.time()
+        if current_time - last_interval_time >= count_interval:
+            lst.append((last_interval_time, interval_number, event_counter))
+            last_interval_time = current_time
+            event_counter = 0  
+            interval_number += 1  
         
-        if len(lst) > last_list_size:
-            increase_counter +=1
-            last_list_size = len(lst)
-            
-        if time.time() - last_increase_time >= count_interval:
-            print("increase counter")
-            increase_counter =0
-            last_increase_time = time.time()
-            
-        time.sleep(runtime - (time.time() - start_time()))
-            
-        
-        pass  # Wait until runtime is reached
+        sleep_time = count_interval - (current_time - last_interval_time)
+        if sleep_time > 0:
+            time.sleep(sleep_time)
 
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup() 
 
+if lst:
+    lst.pop(0)
+    
+tup_new = (lst[0][0]-2,0,0)
+lst.insert(0, tup_new)
+
+# Writing data to CSV
 print("Writing data to CSV...")
-
-col_names = ["Time", "State"]
+col_names = ["Interval Start Time", "Interval Number", "Event Count"]
 df = pd.DataFrame(lst, columns=col_names)
+
 df.to_csv(filename + ".csv", index=False)
-
-
 print("Data written to", filename + ".csv")
